932e02437602246dd0939f5532e6955723d5baf6
Jean Ross <jeancmartins@icloud.com>
2020-10-31 12:02:53 -0300

    Added tests for Auth hook

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -52,6 +52,7 @@ const Input: React.FC<InputProps> = ({
       isFocused={isFocused}
       isFilled={isFilled}
       isErrored={!!error}
+      data-testid="input-container"
     >
       {Icon && <Icon size={20} />}
       <input
